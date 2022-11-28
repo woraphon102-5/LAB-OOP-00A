130f690b3e91f45a9397cdf7411fdcddb617a82b
woraphon <woraphon.p@kkumail.com>
2022-11-28 15:46:56 +0700

    Up date

---
+++ LAB OOP 00A/LAB OOP 00A/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+//name
+Console.WriteLine("Woraphon Pontri");
+//Id
+Console.WriteLine("653450102-5");
+//email
+Console.WriteLine("woraphon.p@kkumail.com");
